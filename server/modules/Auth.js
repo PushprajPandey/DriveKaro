e36e5f794084f7ed7a640af8c6a72badd5c3d78b
Pushpraj Pandey <146978539+PushprajPandey@users.noreply.github.com>
2026-06-17 00:29:24 +0530

    Car_Rental_Platform

--- server/modules/Auth.js
+++ server/modules/Auth.js
@@ -1,10 +1,14 @@
 import bcryptjs from "bcryptjs";
 import { renderFile } from "ejs";
 import jwt from "jsonwebtoken";
-import { resolve } from "path";
+import path from "path";
+import { fileURLToPath } from "url";
 import { ENV } from "../config/index.js";
 import { UserModel } from "../models/index.js";
 import { randomString, sendEmail } from "../utils/index.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const maxAge = 30 * 24 * 60 * 60 * 1000;
 const login = async (req, res) => {
@@ -14,13 +18,14 @@ const login = async (req, res) => {
         console.log("B", req.body);
         const existingUser = await UserModel.findOne({ email });
         if (!existingUser) return res.status(200).send({ message: "User not found!", next: "register" });
-        
+
         // Check if user is verified
-        if (!existingUser.isVerified) return res.status(200).send({ 
-            message: "Please verify your email before logging in. Check your inbox for the verification link.",
-            next: "login" 
-        });
-        
+        if (!existingUser.isVerified)
+            return res.status(200).send({
+                message: "Please verify your email before logging in. Check your inbox for the verification link.",
+                next: "login",
+            });
+
         const passwordValid = await bcryptjs.compare(password, existingUser.password);
         if (!passwordValid) return res.status(200).send({ message: "Password is incorrect!", next: "login" });
         const refreshToken = await randomString(16);
@@ -42,30 +47,30 @@ const register = async (req, res) => {
         const existingUser = await UserModel.findOne({ email });
         if (existingUser && existingUser.isVerified) return res.status(200).send({ message: "User already exists!", next: "login" });
         if (existingUser) await UserModel.findByIdAndDelete(existingUser._id);
-        
+
         // Hash the password
         const hashedPassword = await bcryptjs.hash(password, parseInt(ENV.PASSWORD_SALT));
-        
-        const userCreated = await UserModel.create({ 
-            name, 
-            email, 
+
+        const userCreated = await UserModel.create({
+            name,
+            email,
             password: hashedPassword,
             phone,
-            address
+            address,
         });
-        
+
         if (!userCreated)
             return res
                 .status(200)
                 .send({ message: "We are unable to register new users at the moment! Please contact Admin.", next: "register" });
         const token = jwt.sign({ email, id: userCreated._id }, ENV.VERIFICATION_TOKEN, { expiresIn: "24h" });
-        const path = resolve("templates/verifyUser.ejs");
+        const templatePath = path.join(__dirname, "../templates/verifyUser.ejs");
         const urlPath = `${ENV.CLIENT_URL}${ENV.VERIFICATION_URL}/${token}`;
         console.log(urlPath);
-        const htmlData = await renderFile(path, {
+        const htmlData = await renderFile(templatePath, {
             name,
             urlPath,
-            msg: "You're almost there to start enjoying VeloRent. Simply click the link below to verify your email address and get started.",
+            msg: "You're almost there to start enjoying DriveKaro. Simply click the link below to verify your email address and get started.",
         });
         sendEmail(email, "Verify Email Address", htmlData);
         res.status(200).send({ message: "We have sent an email that contains link to complete your registration.", next: "register" });
@@ -82,10 +87,10 @@ const forgotPassword = async (req, res) => {
         const existingUser = await UserModel.findOne({ email });
         if (!existingUser) return res.status(200).send({ message: "User not found!", next: "register" });
         const token = jwt.sign({ email, id: existingUser._id, reset: true }, ENV.VERIFICATION_TOKEN, { expiresIn: "24h" });
-        const path = resolve("templates/verifyUser.ejs");
+        const templatePath = path.join(__dirname, "../templates/verifyUser.ejs");
         const urlPath = `${ENV.CLIENT_URL}${ENV.VERIFICATION_URL}/${token}`;
         console.log(urlPath);
-        const htmlData = await renderFile(path, {
+        const htmlData = await renderFile(templatePath, {
             name: existingUser.name,
             urlPath,
             msg: "We have received a request to reset the password for your account. Simply click the link below to verify your email address and reset password.",
@@ -139,23 +144,23 @@ const logout = async (req, res) => {
     console.log("Logout requested!");
     try {
         const userId = req.body.id;
-        
+
         // Clear the refresh token in the database
         await UserModel.findByIdAndUpdate(userId, { refreshToken: null });
-        
+
         // Clear cookies
-        res.clearCookie("AT", { 
-            origin: ENV.CLIENT_URL, 
-            sameSite: "none", 
-            httpOnly: true, 
-            secure: true 
+        res.clearCookie("AT", {
+            origin: ENV.CLIENT_URL,
+            sameSite: "none",
+            httpOnly: true,
+            secure: true,
         });
-        res.clearCookie("ATCheck", { 
-            origin: ENV.CLIENT_URL, 
-            sameSite: "none", 
-            secure: true 
+        res.clearCookie("ATCheck", {
+            origin: ENV.CLIENT_URL,
+            sameSite: "none",
+            secure: true,
         });
-        
+
         res.status(200).send({ message: "Logged out successfully" });
     } catch (err) {
         console.error(err);
