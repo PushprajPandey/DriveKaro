4598a768e064677d8d6d46973ccf0abda1be333c
Pushpraj Pandey <146978539+PushprajPandey@users.noreply.github.com>
2026-06-16 22:33:29 +0530

    Car_Rental_Platform

--- server/modules/Car.js
+++ server/modules/Car.js
@@ -3,55 +3,58 @@ import Stripe from "stripe";
 import { ENV } from "../config/index.js";
 import { BookingModel, CarModel, UserModel } from "../models/index.js";
 
-const getAllCars = async (req, res) => {
+const getAllCars = async (req, res, next) => {
     console.log("Get all cars requested!");
-    let { searchText, fuel, numSeats, minPrice, maxPrice } = req.query;
-    const matchConditions = {};
+    try {
+        let { searchText, fuel, numSeats, minPrice, maxPrice } = req.query;
+        const matchConditions = {};
 
-    if (searchText) matchConditions.name = { $regex: searchText, $options: "i" };
-    if (fuel) matchConditions.fuel = fuel;
-    if (numSeats) matchConditions.seats = parseInt(numSeats);
-    if (minPrice || maxPrice) {
-        matchConditions.rent = {};
-        if (minPrice) matchConditions.rent.$gte = parseInt(minPrice);
-        if (maxPrice) matchConditions.rent.$lte = parseInt(maxPrice);
-    }
-    // console.log("MC", matchConditions);
-    const cars = await CarModel.aggregate([
-        { $match: matchConditions },
-        {
-            $lookup: {
-                from: "bookings",
-                localField: "_id",
-                foreignField: "carId",
-                as: "bookingDetails",
+        if (searchText) matchConditions.name = { $regex: searchText, $options: "i" };
+        if (fuel) matchConditions.fuel = fuel;
+        if (numSeats) matchConditions.seats = parseInt(numSeats);
+        if (minPrice || maxPrice) {
+            matchConditions.rent = {};
+            if (minPrice) matchConditions.rent.$gte = parseInt(minPrice);
+            if (maxPrice) matchConditions.rent.$lte = parseInt(maxPrice);
+        }
+        const cars = await CarModel.aggregate([
+            { $match: matchConditions },
+            {
+                $lookup: {
+                    from: "bookings",
+                    localField: "_id",
+                    foreignField: "carId",
+                    as: "bookingDetails",
+                },
             },
-        },
-        {
-            $project: {
-                id: { $toString: "$_id" },
-                name: 1,
-                imgSrc: "$image",
-                seats: 1,
-                rent: 1,
-                fuel: 1,
-                bookingDetails: {
-                    $map: {
-                        input: "$bookingDetails",
-                        as: "booking",
-                        in: {
-                            from: "$$booking.from",
-                            to: "$$booking.to",
+            {
+                $project: {
+                    id: { $toString: "$_id" },
+                    name: 1,
+                    imgSrc: "$image",
+                    seats: 1,
+                    rent: 1,
+                    fuel: 1,
+                    bookingDetails: {
+                        $map: {
+                            input: "$bookingDetails",
+                            as: "booking",
+                            in: {
+                                from: "$$booking.from",
+                                to: "$$booking.to",
+                            },
                         },
                     },
+                    _id: 0,
                 },
-                _id: 0,
             },
-        },
-        { $limit: 20 }  // Adjust the limit value as needed
-    ]);
-    // console.log(cars.length);
-    res.status(200).send({ data: cars });
+            { $limit: 20 },
+        ]);
+        res.status(200).send({ data: cars });
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 };
 
 const newBooking = async (req, res) => {
@@ -94,42 +97,47 @@ const newBooking = async (req, res) => {
     }
 };
 
-const retryBooking = async (req, res) => {
+const retryBooking = async (req, res, next) => {
     console.log("Retry Booking");
-    const { id } = req.params;
-    const bookingDetails = await BookingModel.findById(id);
-    if (!bookingDetails) return res.status(200).send({ message: "Invalid booking", next: "home" });
-    const startTime = dayjs(bookingDetails.from);
-    const endTime = dayjs(bookingDetails.to);
-    const bookingHours = endTime.diff(startTime, "hours");
-    const [carDetails, userDetails] = await Promise.all([
-        CarModel.findById(bookingDetails.carId, { name: 1, rent: 1 }),
-        UserModel.findById(bookingDetails.userId, { email: 1 }),
-    ]);
+    try {
+        const { id } = req.params;
+        const bookingDetails = await BookingModel.findById(id);
+        if (!bookingDetails) return res.status(200).send({ message: "Invalid booking", next: "home" });
+        const startTime = dayjs(bookingDetails.from);
+        const endTime = dayjs(bookingDetails.to);
+        const bookingHours = endTime.diff(startTime, "hours");
+        const [carDetails, userDetails] = await Promise.all([
+            CarModel.findById(bookingDetails.carId, { name: 1, rent: 1 }),
+            UserModel.findById(bookingDetails.userId, { email: 1 }),
+        ]);
 
-    const stripe = new Stripe(ENV.STRIPE_KEY);
-    const session = await stripe.checkout.sessions.create({
-        line_items: [
-            {
-                price_data: {
-                    currency: "inr",
-                    product_data: {
-                        name: carDetails.name,
-                        description: `Booking from ${startTime} to ${endTime}`,
+        const stripe = new Stripe(ENV.STRIPE_KEY);
+        const session = await stripe.checkout.sessions.create({
+            line_items: [
+                {
+                    price_data: {
+                        currency: "inr",
+                        product_data: {
+                            name: carDetails.name,
+                            description: `Booking from ${startTime} to ${endTime}`,
+                        },
+                        unit_amount: carDetails.rent * 100,
                     },
-                    unit_amount: carDetails.rent * 100,
+                    quantity: bookingHours,
                 },
-                quantity: bookingHours,
-            },
-        ],
-        customer_email: userDetails.email,
-        mode: "payment",
-        success_url: `${ENV.CLIENT_URL}${ENV.PAYMENT_CONF_URL}?success=true`,
-        cancel_url: `${ENV.CLIENT_URL}${ENV.PAYMENT_CONF_URL}?success=false`,
-    });
-    await BookingModel.findByIdAndUpdate(id, { paymentId: session.id, status: "pending" });
-    res.cookie("SID", session.id, { origin: ENV.CLIENT_URL, sameSite: "none", httpOnly: true, secure: true, maxAge: 15 * 60 * 1000 });
-    res.status(200).send({ url: session.url, next: "redirect" });
+            ],
+            customer_email: userDetails.email,
+            mode: "payment",
+            success_url: `${ENV.CLIENT_URL}${ENV.PAYMENT_CONF_URL}?success=true`,
+            cancel_url: `${ENV.CLIENT_URL}${ENV.PAYMENT_CONF_URL}?success=false`,
+        });
+        await BookingModel.findByIdAndUpdate(id, { paymentId: session.id, status: "pending" });
+        res.cookie("SID", session.id, { origin: ENV.CLIENT_URL, sameSite: "none", httpOnly: true, secure: true, maxAge: 15 * 60 * 1000 });
+        res.status(200).send({ url: session.url, next: "redirect" });
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 };
 
 const confirmBooking = async (req, res) => {
